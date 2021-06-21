81d3b1ef1e30ada9988682978ef67399b8057997
Brian <brianjmorrissey@gmail.com>
2021-06-21 08:05:48 -0500

    password strength blurry background tutorial via traversy media

---
+++ script.js
@@ -0,0 +1,15 @@
+const background = document.getElementById('background');
+const password = document.getElementById('password');
+let passlength = 0;
+
+password.addEventListener('input', (e) => {
+    const input = e.target.value;
+    const length = input.length;
+    const blurValue = 20 - (length*2);
+    background.style.filter = `blur(${blurValue}px)`
+})
+
+
+
+
+
